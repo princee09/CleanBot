5e36a2eec3bb3f095a7f233f50ddcd6989727298
princee09 <141345546+princee09@users.noreply.github.com>
2025-05-05 02:02:58 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -295,6 +295,10 @@ app.put('/api/user/sessions/:sessionId/rename', async (req, res) => {
 app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
