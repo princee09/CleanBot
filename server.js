f08bb5213f01c31b7442d5f9795292dd9e4b044e
princee09 <141345546+princee09@users.noreply.github.com>
2025-05-05 12:50:25 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -299,6 +299,6 @@ app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
 
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
